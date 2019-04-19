0eac181ac5720a498344b8037339243b2013acf3
Adam Ashenfelter <ashenfad@bigml.com>
2019-04-18 19:59:57 -0700

    Tmp commit

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :aliases {"scrape" ["run" "-m" "partisanship.data.scrape"]
             "graph" ["run" "-m" "partisanship.graph"]
             "dygraph" ["run" "-m" "partisanship.dygraph"]}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :mirrors {"clojure" {:url "https://build.clojure.org/releases/"}
+          "clojure-snapshots" {:url "https://build.clojure.org/snapshots/"}}
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.csv "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [incanter "1.3.0-SNAPSHOT"]
+                 ;; [incanter "1.3.0-SNAPSHOT"]
                  [clj-time "0.3.3"]
                  [enlive "1.0.0"]])
--- src/partisanship/data/scrape_house.clj
+++ src/partisanship/data/scrape_house.clj
@@ -12,21 +12,27 @@
   (map html/text (html/select res pattern)))
 
 (defn scraper [year vote]
-  (let [res (html/html-resource (java.net.URL. (to-url year vote)))
-        parties (pattern res [:party])
-        yeas (butlast (pattern res [:yea-total]))
-        nays (butlast (pattern res [:nay-total]))
-        date (first (pattern res [:action-date]))
-        outcome (apply merge (map (fn [party yea nay]
-                                    {(keyword (str (first party)))
-                                     {:yea (Integer/parseInt yea)
-                                      :nay (Integer/parseInt nay)}})
-                                  parties yeas nays))]
-    (when date
-      (println "House -" year vote)
-      (if (not (empty? outcome))
-        {:year year
-         :vote vote
-         :date (tf/unparse vote/date-fmt (tf/parse in-fmt date))
-         :outcome outcome}
-        {}))))
+  (println "ADAM - house " year vote (to-url year vote))
+  (try
+    (let [_ (println "ADAM --- trying url...")
+          _ (println (to-url year vote))
+          res (html/html-resource (java.net.URL. (to-url year vote)))
+          parties (pattern res [:party])
+          yeas (butlast (pattern res [:yea-total]))
+          nays (butlast (pattern res [:nay-total]))
+          date (first (pattern res [:action-date]))
+          outcome (apply merge (map (fn [party yea nay]
+                                      {(keyword (str (first party)))
+                                       {:yea (Integer/parseInt yea)
+                                        :nay (Integer/parseInt nay)}})
+                                    parties yeas nays))]
+      (println "ADAM - date: " date)
+      (when date
+        (println "House -" year vote)
+        (if (not (empty? outcome))
+          {:year year
+           :vote vote
+           :date (tf/unparse vote/date-fmt (tf/parse in-fmt date))
+           :outcome outcome}
+          {})))
+    (catch Exception _ nil)))
